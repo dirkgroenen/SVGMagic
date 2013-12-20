9261fcd3eb576031f63fe41fb9b40f2e8b8aa26b
dirkgroenen <dirk_groenen@live.nl>
2013-12-20 10:21:51 +0100

    Add license

--- SVGMagic.js
+++ SVGMagic.js
@@ -1,3 +1,21 @@
+/*
+    It's a simple jQuery plugin that searchs for SVG images on your website and creates PNG versions on the run. 
+    Meaning that you don't have to create different versions of the images yourself.
+    Copyright (C) 2013 - Dirk Groenen [Bitlabs Development]
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 (function( $ ){
     $.fn.svgmagic = function(givenoptions) {	
         var defaultoptions = {
