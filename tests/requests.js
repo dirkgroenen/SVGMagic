12df7e66edf8a1c2e7da67d56cf200187a41b80a
Dirk Groenen <dirk@bitlabs.nl>
2014-11-21 15:03:37 +0100

    add test travis tests

---
+++ tests/requests.js
@@ -0,0 +1,14 @@
+var page = require('webpage').create(),
+    server = 'http://bitlabs.nl/svgmagic/converter/3/',
+    data = 'secure=false&dumpcache=false&svgsources[]=http://dev.svgmagic.bitlabs.nl/Fstyle/resizeicon.svg&version=3&origin=dev.svgmagic.bitlabs.nl';
+
+page.open(server, "get", data, function (status) {
+    if (status !== 'success') {
+        console.log('Unable to post');
+    } else {
+        var response = JSON.parse(page.plainText);
+
+        
+    }
+    phantom.exit();
+});
